5047fd596c701adc5bcfcfb42308d71819f54592
Dimitrios Sourlantzis <jem2010@windowslive.com>
2023-02-23 22:04:10 +0200

    more tests

--- tests/dashboard.test.js
+++ tests/dashboard.test.js
@@ -1,11 +1,11 @@
 const test = require('ava').default;
 const got = require('got');
-const Dashboard = require('../src/routes/dashboards')
 const listen = require('test-listen');
 const http = require('http');
+const mongoose = require('mongoose');
+const Dashboard = require('../src/routes/dashboards');
 const app = require('../src/index');
 const {jwtSign} = require('../src/utilities/authentication/helpers');
-const mongoose = require('mongoose');
 
 test.before(async (t) => {
   t.context.server = http.createServer(app);
@@ -22,24 +22,36 @@ test.after.always((t) => {
   t.context.server.close();
 });
 
+// This test checks if the GET /dashboards endpoint returns the correct response and status code
 test('GET /dashboards returns correct response and status code', async (t) => {
+// Create a JWT token with user ID 1
   const token = jwtSign({id: 1});
+  // Send a GET request to the /dashboards endpoint with the JWT token in the Authorization header
   const {
     statusCode,
     body
-  } = await t.context.got(`dashboards`, {
-    headers: {authorization: `Bearer ${token}`}
-  });
-  t.is(statusCode, 200);
-  t.truthy(body.success);
-});
+  } = await t.context.got('dashboards', {headers: {authorization: `Bearer ${token}`}});
 
+  // Assert that the response status code is 200
+  t.is(statusCode, 200);
+  // Assert that the response body contains a truthy value for the "success" key
+  t.truthy(body.success);
+})
+;
+
+// This test checks if the GET /dashboards endpoint returns the correct response and status code
 test('GET /dashboards returns 404 when dashboard not found', async (t) => {
+  // Create a JWT token with user ID 1
   const token = jwtSign({id: 1});
-  const {statusCode, body} = await t.context.got(`dashboards`, {
-    headers: {authorization: `Bearer ${token}`}
-  });
+  // Send a GET request to the /dashboards endpoint with the JWT token in the Authorization header
+  const {
+    statusCode,
+    body
+  } = await t.context.got('dashboards', {headers: {authorization: `Bearer ${token}`}});
+
+  // Assert that the response status code is 404
   t.is(statusCode, 404);
+  // Assert that the response body contains a truthy value for the "success" key
   t.truthy(body.success);
 });
 
@@ -47,13 +59,12 @@ test('POST /create-dashboard creates a new dashboard', async (t) => {
   const token = jwtSign({id: 1});
 
   // Send request to create a new dashboard
-  const {statusCode, body} = await t.context.got.post('create-dashboard', {
-    json: {
-      name: 'Test Dashboard'
-    },
-    headers: {
-      Authorization: `Bearer ${token}`
-    }
+  const {
+    statusCode,
+    body
+  } = await t.context.got.post('create-dashboard', {
+    json: {name: 'Test Dashboard'},
+    headers: {Authorization: `Bearer ${token}`}
   });
 
   // Verify that the response has a success status code
@@ -65,4 +76,24 @@ test('POST /create-dashboard creates a new dashboard', async (t) => {
     name: 'Test Dashboard'
   });
   t.truthy(dashboard);
+});
+
+test('POST /delete-dashboard deletes dashboard and returns success response', async (t) => {
+  // Generate a JWT token for a user with ID 1
+  const token = jwtSign({id: 1});
+
+  // Send a request to delete a dashboard with ID 123
+  const {
+    statusCode,
+    body
+  } = await t.context.got.post('delete-dashboard', {
+    headers: {authorization: `Bearer ${token}`},
+    json: {id: 123}
+  });
+
+  // Assert that the response status code is 200
+  t.is(statusCode, 200);
+
+  // Assert that the response body contains a truthy success property
+  t.truthy(body.success);
 });
