157aa5261bd330272d6e924e57f4d0e537b6410f
Dimitrios Sourlantzis <jem2010@windowslive.com>
2023-02-24 22:18:42 +0200

    add comments to mongoose

--- src/config/mongoose.js
+++ src/config/mongoose.js
@@ -5,13 +5,15 @@ const mongooseOptions = {
   useCreateIndex: true,
   useFindAndModify: false,
   useUnifiedTopology: true,
-  poolSize: 100,
-  keepAlive: true,
-  keepAliveInitialDelay: 300000
+  poolSize: 100, // maximum number of sockets that may be opened for the connection
+  keepAlive: true, // keep the connection alive by sending keepAlive messages every 30 seconds
+  keepAliveInitialDelay: 300000 // initial delay before sending the first keepAlive message
 };
 const mongodbUri = process.env.MONGODB_URI;
   
 module.exports = () => {
   // eslint-disable-next-line no-console
-  mongoose.connect(mongodbUri, mongooseOptions).catch(console.error);
+    // Connect to the MongoDB database using the MONGODB_URI environment variable and Mongoose options
+    mongoose.connect(process.env.MONGODB_URI, mongooseOptions)
+    .catch(console.error)
 };
